d4d946cfc0cdaac60db6160c977dd74ea7189a24
Fakedrift <127430703+Faked2378@users.noreply.github.com>
2024-02-12 20:05:12 +0000

    Added new feature

--- src/main/java/io/github/Faked2378/bang/bang.java
+++ src/main/java/io/github/Faked2378/bang/bang.java
@@ -1,5 +1,17 @@
 package io.github.Faked2378.bang;
 
+import io.github.Faked2378.bang.common.item.C4Item;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+import net.minecraft.entity.player.PlayerEntity;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
@@ -10,9 +22,11 @@ public class bang implements ModInitializer {
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("BLING BANG BANG");
+	public static final Item C4_ITEM = new C4Item(new Item.Settings().maxCount(16).rarity(Rarity.valueOf("EPIC")));
 
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "c4_item"), C4_ITEM);
 	}
 }
